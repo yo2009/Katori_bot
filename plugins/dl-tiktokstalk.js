22a2c853b0ed13a4243ba39569df780f53e056f6
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-02-19 21:47:10 -0300

    DyluxBot • DyluxFG

--- plugins/dl-tiktokstalk.js
+++ plugins/dl-tiktokstalk.js
@@ -1,23 +1,22 @@
+import axios from 'axios'
 
-import fetch from 'node-fetch'
-let handler = async (m, { conn, text, args }) => {
-	
-  if (!text) throw `✳️ ${mssg.noUsername}`
-  
-  try {  	
-  let pon = await fetch(global.API('fgmods', '/api/search/ttstalk', { username: args[0] }, 'apikey'))
-    let res = await pon.json()
+let handler = async (m, { conn, text, args }) => {	
+ if (!text) throw `✳️ ${mssg.noUsername}`  
+ try {  	
+  let pon = await axios.get(`https://mahiru-shiina.boxmine.xyz/stalk/ttstalk?username=${text}`)
+  let res = await pon.data;
     
   let txt = `
 ┌──「 *TIKTOK STALK* 
-▢ *🔖${mssg.name}:* ${res.result.username}
-▢ *🔖${mssg.username}:* ${args[0]}
-▢ *👥${mssg.followers}:* ${res.result.followers}
-▢ *🫂${mssg.follows}:* ${res.result.following}
-▢ *📌${mssg.desc}:* ${res.result.description}
-▢ *🔗${mssg.link}:* https://tiktok.com/@${args[0]}
+▢ *🔖${mssg.name}:* ${res.data.nickname}
+▢ *🔖${mssg.username}:* ${res.data.username}
+▢ *👥${mssg.followers}:* ${res.data.followers}
+▢ *🫂${mssg.follows}:* ${res.data.following}
+▢ *📌${mssg.desc}:* ${res.data.description}
+▢ *🔗${mssg.link}:* https://tiktok.com/@${res.data.username}
 └────────────`
-  await conn.sendFile(m.chat, res.result.profile, 'tt.png', txt, m)
+	 
+await conn.sendFile(m.chat, res.data.avatar, 'tt.png', txt, m)
 } catch {
   m.reply(`✳️ ${mssg.error}`)
 }
@@ -27,4 +26,4 @@ handler.help = ['tiktokstalk']
 handler.tags = ['dl']
 handler.command = /^t(tstalk|iktokstalk)$/i
 
-export default handler
+export default handler;
