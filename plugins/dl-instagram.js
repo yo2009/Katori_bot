847a47ffb733c03878bd5b65aa73f63c83e288c8
FG98 <81941690+FG98F@users.noreply.github.com>
2025-02-20 03:00:37 -0300

    Update dl-instagram.js

--- plugins/dl-instagram.js
+++ plugins/dl-instagram.js
@@ -1,4 +1,4 @@
-
+ 
 import fetch from 'node-fetch';
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     if (!args[0]) throw `✳️ ${mssg.useCmd}\n *${usedPrefix + command}* https://www.instagram.com/p/CYHeKxyMj-J/?igshid=YmMyMTA2M2Y=`
@@ -6,13 +6,12 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
 
 try {
     let res = await fetch(global.API('fgmods', '/api/downloader/igdl', { url: args[0] }, 'apikey'))
-    if (!res.ok) throw `❎ ${mssg.error} ` 
+    if (!res.ok) throw `❎ ${mssg.error}` 
     let data = await res.json()
 
-    for (let item of data.result) {
-        conn.sendFile(m.chat, item.url, 'igdl.jpg', `✅ ${mssg.result}`, m)
+    for (let item of data.result.url) {
+        conn.sendFile(m.chat, item, 'igdl.jpg', `✅ ${mssg.result}`, m)
     }
-  
   
     } catch (error) {
         m.reply(`❎ ${mssg.error}`)
