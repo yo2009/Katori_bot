56ab93e19d7c0446f73fc7b691e4d59329e74698
ゞ 𝐆𝐚𝐛𝐫𝐢𝐞𝐥 𝐒𝐤 ぁ <148560941+glytglobal@users.noreply.github.com>
2025-02-23 18:41:13 -0300

    Dylux - OpenAi
    #Actualización
    - Se actualizo el sub-dominio de Skynex Api
    - Se agregó un nuevo parámetro "apikey"

--- plugins/tools-openai.js
+++ plugins/tools-openai.js
@@ -3,9 +3,9 @@ let handler = async (m, { conn, text }) => {
 if (!text) throw `✳️ ${mssg.notext}`
 m.react('💬')
 
- let syst = `Eres Senna Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
+    let syst = `Eres Senna Bot, un gran modelo de lenguaje entrenado por OpenAI. Siga cuidadosamente las instrucciones del usuario. Responde usando Markdown.`
      try {
-        let gpt = await fetch(`https://mahiru-shiina.boxmine.xyz/docs/ai/myprompt?text=${text}&prompt=${syst}`)
+       let gpt = await fetch(`https://skynex.boxmine.xyz/docs/ai/myprompt?text=${text}&prompt=${syst}&apikey=Dylux`)
         let res = await gpt.json()
         await m.reply(res.answer, null, fwc)
 	} catch {
